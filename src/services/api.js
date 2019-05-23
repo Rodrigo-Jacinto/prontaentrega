1b273943a0ad75df364447c3e5fe9a866e35771d
Rodrigo Jacinto da Silva <rodrigojacinto16@gmail.com>
2019-05-23 00:37:03 -0300

    ajustando url da apii

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const  api = axios.create({baseURL: 'https://prontaentrega-api.herokuapp.com/'});
+const  api = axios.create({baseURL: 'https://prontaentrega-api.herokuapp.com'});
 
 
 export default api;
